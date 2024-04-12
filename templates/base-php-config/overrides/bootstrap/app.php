a8112b01b50289bd35b1b37a0f5570a65843794b
Marius <marius@coddin.nl>
2024-04-12 09:32:22 +0200

    Update to Laravel 11 and newer standards

---
+++ templates/base-php-config/overrides/bootstrap/app.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+        then: function () {
+            if (config('app.env') === 'local' && \file_exists(base_path('routes/test.php'))) {
+                Route::middleware(['web'])
+                    ->prefix('test')
+                    ->group(base_path('routes/test.php'));
+            }
+        },
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
